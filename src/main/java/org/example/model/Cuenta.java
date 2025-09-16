54700336c36f216038d43504ee6badc499271821
DEValdivieso <omarjet30@gmail.com>
2025-09-16 18:59:58 -0500

    add thrwo exception

--- src/main/java/org/example/model/Cuenta.java
+++ src/main/java/org/example/model/Cuenta.java
@@ -1,5 +1,7 @@
 package org.example.model;
 
+import org.example.exception.DineroInsuficienteException;
+
 import java.math.BigDecimal;
 
 public class Cuenta {
@@ -26,5 +28,31 @@ public class Cuenta {
 
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
+    }
+
+    public void debito(BigDecimal monto) {
+        BigDecimal nuevoSaldo = saldo.subtract(monto);
+        if(nuevoSaldo.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new DineroInsuficienteException("Dinero insuficiente");
+        }
+        saldo = nuevoSaldo;
+    }
+
+    public void credito(BigDecimal monto) {
+        saldo = saldo.add(monto);
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null){
+            return false;
+        }
+        Cuenta cuenta = (Cuenta) obj;
+        if(this.persona == null || this.saldo == null){
+            return false;
+        }
+        return this.persona.equals(cuenta.getPersona()) && this.saldo.equals(cuenta.getSaldo());
+        //return super.equals(obj);
     }
 }
--- src/test/java/org/example/model/CuentaTest.java
+++ src/test/java/org/example/model/CuentaTest.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 //import org.junit.jupiter.api.Assertions;
+import org.example.exception.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -10,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CuentaTest {
 
     @Test
-    void test(){
+    void testNombreCuenta(){
 
         Cuenta cuenta = new Cuenta("Diego", new BigDecimal("30.12"));
        // cuenta.setPersona("Diego");
@@ -26,6 +27,42 @@ class CuentaTest {
         Cuenta cuenta = new Cuenta("Diego", new BigDecimal("15.22"));
         assertEquals(15.22, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO)<0);
+    }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("Diego", new BigDecimal("15.22"));
+        Cuenta cuenta2 = new Cuenta("Diego", new BigDecimal("15.22"));
+
+        assertEquals(cuenta, cuenta2);
+    }
+
+    @Test
+    void testDebitoCuenta(){
+        Cuenta cuenta = new Cuenta("Diego", new BigDecimal("15.22"));
+        cuenta.debito(new BigDecimal("10"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(5, cuenta.getSaldo().intValue());
+        assertEquals("5.22", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testCreditoCuenta(){
+        Cuenta cuenta = new Cuenta("Diego", new BigDecimal("15.22"));
+        cuenta.credito(new BigDecimal("10"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(25, cuenta.getSaldo().intValue());
+        assertEquals("25.22", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta(){
+        Cuenta cuenta = new Cuenta("Diego", new BigDecimal("15.22"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, ()-> cuenta.debito(new BigDecimal("16")));
+        String actual = exception.getMessage();
+        String esperado = "Dinero insuficiente";
+        assertEquals(esperado, actual);
+
     }
 
 }
